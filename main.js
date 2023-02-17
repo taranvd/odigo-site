3dafb353754ec3ae107ef2a0727435d943c5c855
taranvd <apple.vlad.taran@gmail.com>
2023-02-17 16:19:43 +0200

    start

---
+++ main.js
@@ -0,0 +1,46 @@
+
+
+(function () {
+    const header = document.querySelector('header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header-active');
+        } else {
+            header.classList.remove('header-active');
+        }
+    };
+}());
+
+// Burger handler
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header-nav');
+    const menuCloseItem = document.querySelector('.header-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header-nav-active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header-nav-active');
+    })
+}());
+
+
+
+
+
+
+// // Burger handler
+
+// (function () {
+//     const burgerItem = document.querySelector('.burger'); 
+//     const menu = document.querySelector('.header__nav');
+//     const menuCloseItem = document.querySelector('.header-nav-close');
+//         burgerItem.addEventListener('click', () => {
+//             menu.classList.add('header__nav-active');
+//         });
+//          menuCloseItem.addEventListener('click', () => {
+//             menu.classList.remove('header__nav-active');
+//         })
+    
+// }());
